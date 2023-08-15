918198b657e71c61d28378985a7fdb5c0ffde548
Ossy Lab <ossyisibor@gmail.com>
2023-08-15 06:06:39 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuilErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
