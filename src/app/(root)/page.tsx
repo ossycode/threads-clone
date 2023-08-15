d78e70d5bab892f9251ea9b6afaa1d759d2fcb8f
Ossy Lab <ossyisibor@gmail.com>
2023-08-15 06:29:23 +0100

    config

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   return (
     <>
       {/* <UserButton afterSignOutUrl="/" /> */}
-      <h1 className="head-text text-left">Home</h1>
+      <h1 className="head-text text-left text-heading1-semibold">Home</h1>
 
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
